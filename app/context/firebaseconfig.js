ab697e0153b265bb9e276560923c44cbbe69b473
Tomas Pineiro <tomaspineiro1@outlook.es>
2025-01-29 22:45:30 -0500

    mas cambios

---
+++ app/context/firebaseconfig.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAsPUT3O03xb4iJmpn1x8xUwO37REFKX7A",
+  authDomain: "coder-nextjs-60e1a.firebaseapp.com",
+  projectId: "coder-nextjs-60e1a",
+  storageBucket: "coder-nextjs-60e1a.firebasestorage.app",
+  messagingSenderId: "1079699828978",
+  appId: "1:1079699828978:web:a3b583ea9cd6175927a06b"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
